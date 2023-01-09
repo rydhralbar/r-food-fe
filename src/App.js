d801560dab594e9139a03993807295b685b65047
Riyadh <albarrriyadh1112@gmail.com>
2023-01-09 15:42:32 +0800

    [ADD] Add React JS Version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import HomeLogged from "./pages/HomeLogged";
+import DetailLogged from "./pages/DetailLogged";
+import ProfileMyRecipe from "./pages/ProfileMyRecipe";
+import ProfileSavedRecipe from "./pages/ProfileSavedRecipe";
+import DetailGuest from "./pages/DetailGuest";
+import ForgotPassword from "./pages/ForgotPassword";
+import ForgotVerification from "./pages/ForgotVerification";
+import ProfileLikedRecipe from "./pages/ProfileLikedRecipe";
+// import Maintenance from "./pages/Maintenance";
+import CreateNewPassword from "./pages/CreateNewPassword";
+import AddRecipe from "./pages/AddRecipe";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const maintenance = ["/forgot/verification"]
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "login",
+      element: <Login />,
+    },
+    {
+      path: "signup",
+      element: <Register />,
+    },
+    {
+      path: "home",
+      element: <HomeLogged />
+    },
+    {
+      path: "/detail",
+      element: <DetailGuest />
+    },
+    {
+      path: "/home/detail",
+      element: <DetailLogged />
+    },
+    {
+      path: "profile",
+      element: <ProfileMyRecipe />
+    },
+    {
+      path: "profile/saved-recipe",
+      element: <ProfileSavedRecipe />
+    },
+    {
+      path: "profile/liked-recipe",
+      element: <ProfileLikedRecipe />
+    },
+    {
+      path: "/add-recipe",
+      element: <AddRecipe />
+    },
+    {
+      path: "forgot",
+      element: <ForgotPassword />
+    },
+    {
+      path: "forgot/verification",
+      element: <ForgotVerification />
+    },
+    {
+      path: "/create-password",
+      element: <CreateNewPassword />
+    }
+  ])
+
+  // if(true) {
+  //   return <Maintenance />;
+  // } else {
+  //   return <RouterProvider router={router} />;
+  // }
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
